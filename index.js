03e68092196e7658166408d2d18141094e6e89f1
abhik <abhikchowdhury.20@nshm.edu.in>
2023-03-08 14:02:45 +0530

    The Backend of friends Chat

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes")
+const messagesRoutes = require("./routes/messagesRoutes")
+const mongoose = require("mongoose");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+app.use(cors())
+app.use(express.json());
+
+app.use("/api/auth",userRoutes)
+app.use("/api/messages",messagesRoutes)
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("DB Connect Successfull")
+}).catch((err)=>{
+    console.log(err.message)
+})
+
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server Started on PORT ${process.env.PORT}`)
+})
+const io = socket(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        credential:true,
+    },
+});
+global.onlineUsers = new Map();
+
+io.on("connection",(socket)=>{
+    global.chatSocket = socket;
+    socket.on("add-use",(userId)=>{
+        onlineUsers.set(userId,socket.id);
+    });
+
+    socket.on("send-msg",(data)=>{
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-recieve",data.msg);
+        };
+    });
+});
