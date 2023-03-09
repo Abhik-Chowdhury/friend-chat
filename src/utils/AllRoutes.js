a6c2a7b75f5c0ff7b0c54f04cdc5903a43357a7d
Abhik Chowdhury <99788745+Rebel-622@users.noreply.github.com>
2023-03-09 13:29:41 +0530

    Update AllRoutes.js

--- src/utils/AllRoutes.js
+++ src/utils/AllRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://friend-chat-database.onrender.com";
 export const registeRoute = `${host}/api/auth/register`; 
 export const loginRoute = `${host}/api/auth/login`; 
 export const allUserRoute = `${host}/api/auth/allusers`;
